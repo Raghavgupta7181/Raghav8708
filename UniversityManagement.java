b37a7f6ab6bff6abef64a176671cc9016084447b
raghavgupta8708 <raghavgupta8708@gmail.com>
2025-09-21 19:53:37 +0530

    Update UniversityManagement.java

--- UniversityManagement.java
+++ UniversityManagement.java
@@ -1,5 +1,5 @@
 class Student {
- 	private static String universityName = "XYZ University"; 
+ 	private static String universityName = "Princeston University"; 
  	private final int rollNumber; 
  	private String name;
  	private String grade;
@@ -24,9 +24,9 @@ class Student {
  
  public class UniversityManagement {
  	public static void main(String[] args) {
-     	Student stu1 = new Student(21, "Riya", "A");
-     	Student stu2 = new Student(22, "Tarun", "B");
-		Student stu3 = new Student(23, "Suman", "B");
+     	Student stu1 = new Student(21, "JoshAllen", "A");
+     	Student stu2 = new Student(22, "PatrickMahomes", "B");
+		Student stu3 = new Student(23, "TravisKelce", "B");
 
      	stu1.displayStudentDetails();
      	stu2.displayStudentDetails();

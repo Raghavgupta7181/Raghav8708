7008525dcc50fce9b95428f248057e46081eac54
Raghavgupta7181 <161826010+Raghavgupta7181@users.noreply.github.com>
2025-11-09 16:56:24 +0530

    Delete NFL.java

--- NFL.java
+++
@@ -1,7 +0,0 @@
-class NFL {
-    public static void main (String args[]) {
-        System.out.println("Chichago Bears");
-        System.out.println("Cleveland Browns");
-        System.out.print("Arizona Cardinals");
-    }
-}
